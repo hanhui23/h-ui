356343e1194f6bc6a96b6f609d0b763eefaa56d8
hui <125173679@qq.com>
2020-12-04 18:49:04 +0800

    添加测试demo

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,37 @@
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import { defineComponent, h } from 'vue';
+const HelloWorld = defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  setup(props) {
+    return () => {
+      return h('div', props.msg)
+    }
+  }
+})
 describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
+  beforeEach(() => {
+    console.log('before each')
+  })
+  afterEach(() => {
+    console.log('after each')
+  })
+  beforeAll(() => {
+    console.log('before All')
+  })
+  afterAll(() => {
+    console.log('after All')
+  })
+  it("renders props.msg when passed", async() => {
     const msg = "new message";
     const wrapper = shallowMount(HelloWorld, {
       props: { msg }
     });
-    expect(wrapper.text()).toMatch(msg);
-  });
+    await wrapper.setProps({
+      msg: '123'
+    })
+    expect(wrapper.text()).toEqual(msg);
+  })
 });
